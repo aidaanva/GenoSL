c61e851fe2bee44056b69f614714457fa22b9daa
Aida Andrades Valtueña <andrades@shh.mpg.de>
2020-10-09 09:20:03 +0200

    Added table functionality to genotype multiple samples at a time

--- genoSL.R
+++ genoSL.R
@@ -10,21 +10,10 @@ parser <- add_argument(parser, 'input',
                        type='character',
                        nargs=1,
                        help='Path to the snpTable.tsv produced by MultiVCFAnalyzer')
-parser <- add_argument(parser, 'nameForwardReads',
-                       type="character",
-                       nargs=1,
-                       help='Name of the column in snpTable.tsv corresponding to forward reads')
-parser <- add_argument(parser, 'nameReverseReads',
-                       type='character',
-                       nargs=1,
-                       help='Name of the column in snpTable.tsv corresponding to forward reads')
-parser <- add_argument(parser, 'allReads',
+parser <- add_argument(parser, 'table',
                        type = 'character',
-                       nargs=1,
-                       help='Name of the column in snpTable.tsv corresponding to all reads')
-parser <- add_argument(parser, 'sampleName',
-                       type = 'character',
-                       help = 'specify name of the sample, the new column in snpTable and header in fasta file will be sampleName_c')
+                       nargs = 1,
+                       help = 'Path to table.tsv containing genomes to Genotype. See README.md for specific formating')
 parser <- add_argument(parser, '--output',
                        type = 'character',
                        help = 'Specify root of output name, default is snpTable',
@@ -46,8 +35,6 @@ genotyping <- function(df, forward, reverse, aReads, sampleName){
   names <- replace(names, names==forward, "forwardReads")
   names <- replace(names, names==reverse, "reverseReads")
   names <- replace(names, names==aReads, "allReads")
-  names <- str_replace(names, "^X", "")
-  names <- str_replace(names, "^X", "XX")
   colnames(snpTable) <- names
   
   snpTableGenotyped <- snpTable %>%
@@ -55,6 +42,14 @@ genotyping <- function(df, forward, reverse, aReads, sampleName){
       reverseReads == "T" | forwardReads == "T" | allReads == "T" ~ reverseReads,
       forwardReads == "A" | reverseReads == "A" | allReads == "A" ~ forwardReads,
       TRUE ~ allReads))
+  
+  correctGenotype <- paste(sampleName, "c", sep = "_")
+  names<-colnames(snpTableGenotyped)
+  names <- replace(names, names=="forwardReads", forward)
+  names <- replace(names, names=="reverseReads", reverse)
+  names <- replace(names, names=="allReads", aReads)
+  names <- replace(names, names=="genotypedSample", correctGenotype)
+  colnames(snpTableGenotyped) <- names
     return(snpTableGenotyped)
 }
 preparingAllTable <- function(df, sampleName){
@@ -82,6 +77,7 @@ writingFasta <- function(df, output){
   write_tsv(df2, output, col_names = F)
   outputFasta <- paste(output, "fasta", sep = ".")
   system(paste("awk '{print \">\"$1; $1=\"\"; print $0}' OFS=", output, ">", outputFasta))
+  system(paste("rm",output))
   print("Fasta_dp saved")
 }
 fastatoTibble <- function(fasta) {
@@ -98,14 +94,25 @@ fastatoTibble <- function(fasta) {
   return(df_sampleRows)
   print("fastaToTibble Comple")
 }
-snpTable <- read.delim(argv$input, sep = "\t", stringsAsFactors = F)
 
-snpTableGT <- genotyping(snpTable, argv$nameForwardReads, argv$nameReverseReads, argv$allReads, argv$sampleName)
-snpTable_c <- snpTableGT %>%
-  select(-forwardReads, -reverseReads, -allReads) %>%
-  preparingAllTable(argv$sampleName)
+### END FUNCTIONS
 
-snpTable_AllIncluded <- preparingAllTable(snpTableGT, argv$sampleName)
+snpTable <- read.delim(argv$input, sep = "\t", stringsAsFactors = F, check.names = F)
+genoTable <- read.delim(argv$table, sep = "\t", stringsAsFactors = F, check.names = F)
+snpTableGT <- snpTable
+
+for (row in 1:nrow(genoTable)) {
+  nameForwardReads <- genoTable[row, "Forward"]
+  nameReverseReads <- genoTable[row, "Reverse"]
+  allReads <- genoTable[row, "All"]
+  sampleName <- genoTable[row, "sampleName"]
+  snpTableGT <- genotyping(snpTableGT, nameForwardReads, nameReverseReads, allReads, sampleName)
+#  snpTable_c <- snpTableGT %>%
+#    select(-nameForwardReads, -nameReverseReads, -allReads) %>%
+#    preparingAllTable(sampleName)
+  
+#  snpTable_AllIncluded <- preparingAllTable(snpTableGT, sampleName)
+}
 
 
 if(argv$fasta != "None" ){
@@ -120,18 +127,15 @@ if(argv$fasta != "None" ){
     mutate(CorrectedCall=ifelse(Position %in% basesToChange, basesToChange$Call, Call))
   }
 
-allIncluded <- paste(argv$output, argv$sampleName, "allColumns.tsv", sep = "_")
-corrected <- paste(argv$output, argv$sampleName, "c.tsv", sep = "_")
-fastaFile <- paste(argv$output, argv$sampleName, sep = "_")
+allIncluded <- paste(argv$output, "allColumns.tsv", sep = "_")
+corrected <- paste(argv$output, "genotyped.tsv", sep = "_")
+fastaFile <- paste(argv$output, "genotyped", sep = "_")
+
+snpTable_c <- snpTableGT[,!names(snpTableGT) %in% genoTable$All]
+snpTable_c <- snpTable_c[,!names(snpTable_c) %in% genoTable$Reverse]
+snpTable_c <- snpTable_c[,!names(snpTable_c) %in% genoTable$Forward]
 
 writingFasta(snpTable_c, fastaFile)
 
-write_tsv(snpTable_AllIncluded, allIncluded)
+write_tsv(snpTableGT, allIncluded)
 write_tsv(snpTable_c, corrected)
-
-#snpTableForFasta <-snpTable_c %>% gather(Samples,Call, 3:ncol(.))
-#snpTableForFasta$Call = ifelse(snpTableForFasta$Call==".", as.character(snpTableForFasta$Ref),as.character(snpTableForFasta$Call))
-#snpTable_c_fsnpalignment <- snpTableForFasta %>%
-#  spread(Samples, Call)
-#forFasta <- paste(argv$output, argv$sampleName,"c_forfasta.tsv", sep = "")
-#write_tsv(snpTable_c_fsnpalignment, forFasta)
